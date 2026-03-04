6030435a7c2a88abb03e6499e89a161bf6a94cab
AxlvesDev <vinicius.a2@hotmail.com>
2026-03-04 15:51:56 -0300

    metodo create e index das mesas

---
+++ src/routes/tables-sessions-routes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express";
+
+import { TablesSessionsController } from "../controllers/tables_sessions-controller";   
+
+const tablesSessionsRoutes = Router()
+const tablesSessionsController = new TablesSessionsController()
+
+tablesSessionsRoutes.post("/", tablesSessionsController.create)
+tablesSessionsRoutes.get("/", tablesSessionsController.index)
+
+export { tablesSessionsRoutes }
